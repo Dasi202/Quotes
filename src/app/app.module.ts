e754609aeb1fb9451f18834153279637361e7bce
Dasi202 <situmadanlon@gmail.com>
2021-03-29 20:35:35 +0300

    Add src/app/app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { ShowbadgeDirective } from './showbadge.directive';
+import { DayCountPipe } from './day-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent,
+    ShowbadgeDirective,
+    DayCountPipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule,AppRoutingModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
