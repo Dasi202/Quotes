ae8408405cf8dde6bc492281674f54affb393096
Dasi202 <situmadanlon@gmail.com>
2021-03-29 21:01:45 +0300

    Add showbadge directive.spec.ts

---
+++ src/app/showbadge.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ShowbadgeDirective } from './showbadge.directive';
+
+describe('ShowbadgeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ShowbadgeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
